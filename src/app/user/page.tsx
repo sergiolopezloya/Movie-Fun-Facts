2a96d730e6347aa14c41d5b56adaf38ce709da88
Sergio Lopez <sergio@softnya.com>
2025-08-29 17:16:59 -0500

    Set initial formats

---
+++ src/app/user/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold mb-4">User Information</h1>
+      <p>Here we will display user details and movie facts.</p>
+    </div>
+  );
+};
+
+export default UserPage;
